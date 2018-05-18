e3ba364bb087ef0512d531cb33a8bb4e2bfd03f0
fcastaneda9725 <f.castanedat@gmail.com>
2018-05-18 18:37:11 -0500

    Inclusión de Lista dinámica, ajustes de permutación de cadena

--- js/app.js
+++ js/app.js
@@ -1,28 +1,53 @@
 /* Programación en JavaScript */
-var contador = 0;
-var lista = [];
 
 //Variables globales para uso de la app.
-function limpiar() {
-  contador = 0;
-  lista = [];
-  return contador, lista;
+var contador = 0;
+var lista = [];
+/* Código para mostrar contenedor donde se pueden hacer las permutaciones */
+function mostrarApp() {
+  var contenido = document.getElementById('contenedor-app');
+  contenido.classList.toggle('oculto');
+  var boton = document.getElementById('boton-mostrar');
+  if (boton.innerHTML == 'Vamos a Probarlo') {
+    boton.innerHTML = 'Ocultar App';
+  }else {
+    boton.innerHTML = 'Vamos a Probarlo';
+  }
 }
 
+/* Código para las permutaciones */
 function clickBtn() {
   limpiar();
   var cadena = document.getElementById('cadena').value;
   permuta(' ', cadena);
+  imprimir(lista);
 }
 
 function permuta(cad1, cad2) {
   if (cad2.length == 1) {
     contador++;
-    lista.push(contador + '=' + cad1 + cad2);
+    lista.push(contador + ' .-' + cad1 + cad2);
     console.log(lista);
   }
 
   for (var i = 0; i < cad2.length; i++) {
     permuta(cad1 + cad2.charAt(i), cad2.replace(cad2.charAt(i), ''));
   }
+
+}
+
+function imprimir(cadena) {
+  for (var i = 0; i < cadena.length; i++) {
+    var grupo = document.getElementById('lista-dinamica');
+    var l = document.createElement('li');
+    l.appendChild(document.createTextNode(cadena[i]));
+    l.setAttribute('class', 'list-group-item');
+    grupo.appendChild(l);
+  }
+}
+
+function limpiar() {
+  contador = 0;
+  lista = [];
+  return contador, lista;
 }
