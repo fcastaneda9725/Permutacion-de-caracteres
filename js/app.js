ccd02845d8f37ce1e043c62506e3f767103a1475
fcastaneda9725 <f.castanedat@gmail.com>
2018-05-18 17:18:39 -0500

    Inicio de la permutación

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 /* Programación en JavaScript */
+var contador = 0;
+var lista = [];
+
+//Variables globales para uso de la app.
+function limpiar() {
+  contador = 0;
+  lista = [];
+  return contador, lista;
+}
+
+function clickBtn() {
+  limpiar();
+  var cadena = document.getElementById('cadena').value;
+  permuta(' ', cadena);
+}
+
+function permuta(cad1, cad2) {
+  if (cad2.length == 1) {
+    contador++;
+    lista.push(contador + '=' + cad1 + cad2);
+    console.log(lista);
+  }
+
+  for (var i = 0; i < cad2.length; i++) {
+    permuta(cad1 + cad2.charAt(i), cad2.replace(cad2.charAt(i), ''));
+  }
+}
